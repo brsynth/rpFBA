9e9baad3ac72e8f4abff5cd1420da81cbd6495d3
breakthewall <joan.herisson@univ-evry.fr>
2020-12-11 14:03:33 +0100

    chore(args): create separate file to andle cli args

--- rpfba/__init__.py
+++ rpfba/__init__.py
@@ -4,8 +4,8 @@ Created on June 16 2020
 @author: Joan Hérisson
 """
 
-from rpfba.rpFBA import runFBA, runParsimoniousFBA, runFractionReaction
+from rpfba.rpFBA import runFBA, rp_fba, rp_pfba, rp_fraction
 from rpfba.Args  import build_args_parser
 
-__all__ = ["runFBA", "runParsimoniousFBA", "runFractionReaction",
+__all__ = ["runFBA", "rp_fba", "rp_pfba", "rp_fraction",
            "build_args_parser"]
